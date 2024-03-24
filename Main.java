3187bc87599f8edd178ee7fed49e8b75ebe20133
vitorwmoutinho <126618834+vitorwmoutinho@users.noreply.github.com>
2024-03-24 16:29:25 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Ola, Mundo");
-	}
+public class Main {
+    public static void main(String[] args){
+        
+        double n1, n2, n3, n4, res;
+        
+        System.out.print("Informe a nota 1: ");
+        n1 = Double.parseDouble(System.console().readLine());
+        
+        System.out.print("Informe a nota 2: ");
+        n2 = Double.parseDouble(System.console().readLine());
+        
+        System.out.print("Informe a nota 3: ");
+        n3 = Double.parseDouble(System.console().readLine());
+        
+        System.out.print("Informe a nota 4: ");
+        n4 = Double.parseDouble(System.console().readLine());
+        
+        res = (n1 + n2 + n3 + n4) / 4;
+        
+        System.out.printf("Média das notas = %.1f\n", res);
+        
+        // Implementação SE...ENTÃO em JAVA
+        
+        if(res >= 7)
+            System.out.println("Aluno aprovado!");
+            
+        else
+            System.out.println("Aluno reprovado!");
+    }
 }
