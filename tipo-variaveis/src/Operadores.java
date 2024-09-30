bd6da6d9b56dea484b069daff98f7dd3125cae6a
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-09-30 08:16:34 -0300

    Operador ternario

--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -1,18 +1,13 @@
 public class Operadores {
 
     public static void main(String[] args){
-        String concatenacao = "?";
+        int a, b;
+        a = 5;
+        b = 6;
 
-        System.out.println(concatenacao);
+        String resultado = a==b ? "verdadeiro" : "falso";
 
-        concatenacao = 1 + 1 + "1" + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + 1 + 1 + "1" ;
-
-        System.out.println(concatenacao);
-
+        System.out.println(resultado);
 
 
     }
