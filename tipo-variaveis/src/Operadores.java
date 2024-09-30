352c463f596df6d32c6bbdf8c5ca64d2e66a5ec7
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-09-30 08:42:46 -0300

    Condicionais

--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -1,10 +1,21 @@
 public class Operadores {
 
     public static void main(String[] args) throws Exception { 
-        String nomeUm = "Zé";
-        String nomeDois = new String("Zé");
+        boolean condicao1 = true;
 
-        System.out.println(nomeUm.equals(nomeDois));
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2)){
+            System.out.println("As duas condições são verdadeiras");
+
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("FIM");
+
 
     }
     
