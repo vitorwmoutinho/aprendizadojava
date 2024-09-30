f40821e85014b01d852a1edc9a5f200a09980386
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-09-30 10:05:48 -0300

    Criação de smart tv

---
+++ New folder/sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,29 @@
+public class SmartTv {
+    boolean ligada=false;
+    int canal=1;
+    int volume=25;
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentado o volume para: " + volume);
+
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+
+    }
+
+    public void ligar(){
+        ligada = true;
+
+    }
+    public void desligar(){
+        ligada = false;
+
+    } 
+                                   
+
+    
+}
---
+++ New folder/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,21 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual:" + smartTv.canal);
+        System.out.println("Volume atual:" + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+    }
+}
