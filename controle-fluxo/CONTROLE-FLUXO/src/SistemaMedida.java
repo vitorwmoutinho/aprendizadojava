0320fce072b8c6803b80f28940636df88dac4f2a
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-01 15:13:50 -0300

    Aprendendo Switch case

---
+++ controle-fluxo/CONTROLE-FLUXO/src/SistemaMedida.java
@@ -0,0 +1,34 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "P";
+
+        switch (sigla) {
+            case "P":
+                System.out.println("Pequeno");
+                
+                break;
+            
+        
+        
+            case "M":
+                System.out.println("Médio");
+                        
+                break;
+                
+        
+        
+       
+            case "G":
+                System.out.println("Grande");
+                        
+                break;
+
+            default:
+                System.err.println("Indefinido");
+        }
+        
+        
+        }
+
+    }
+    
