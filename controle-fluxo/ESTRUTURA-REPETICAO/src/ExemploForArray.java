0f6db985b02332dab795be52ef81f77e8c11d504
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-01 16:58:33 -0300

    Exemplo de for com array

---
+++ controle-fluxo/ESTRUTURA-REPETICAO/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for(int x = 0; x < alunos.length; x++){
+            System.out.println("Oluno no indice x= " + x + alunos[x]);
+            
+        }
+    }
+    
+}
