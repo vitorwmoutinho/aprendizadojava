acecb1fda7f0723925d2d45ec6fbbc76717804ee
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-01 10:28:35 -0300

    Melhora do fluxo

--- controle-fluxo/CONTROLE-FLUXO/src/CaixaEletronico.java
+++ controle-fluxo/CONTROLE-FLUXO/src/CaixaEletronico.java
@@ -1,10 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 22.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println(" Novo Saldo: "+ saldo);
+        }
+            
+        else
+            System.out.println(" Saldo Insuficiente.");
 
         System.out.println("Saldo igual a: " + saldo);
     }
