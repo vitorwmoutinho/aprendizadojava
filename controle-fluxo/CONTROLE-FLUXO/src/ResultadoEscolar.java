d266bccec22451bbd04104cbfd28de4035ecfc27
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-01 14:21:54 -0300

    Atualização sobre controle de fluxo

--- controle-fluxo/CONTROLE-FLUXO/src/ResultadoEscolar.java
+++ controle-fluxo/CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -1,10 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) throws Exception {
-        int nota = 8;
+        int nota = 10;
 
         if(nota >= 7)
             System.out.println("Aprovado");
-        
+
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Rcuperação");
         else
             System.out.println("Reprovado");
     }
