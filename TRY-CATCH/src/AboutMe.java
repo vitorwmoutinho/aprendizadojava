db87aca3b05dc6ba832a79799faec3d19e009223
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-02 21:05:22 -0300

    Aprendizado Tratamento de exceções

---
+++ TRY-CATCH/src/AboutMe.java
@@ -0,0 +1,27 @@
+// Tratamento de exceções
+
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String [] args){
+
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm");
+    }
+
+}
