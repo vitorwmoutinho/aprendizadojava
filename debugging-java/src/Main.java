8721705affda6a63bb8ad0b842173daf003f7ace
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-03 09:37:55 -0300

    Atualização do aprendizado debugging

--- debugging-java/src/Main.java
+++ debugging-java/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Iniciou do programada meotodo main");
+        System.out.println("Iniciou o programada meotodo main");
+        a();
+        System.out.println("Encerrou o programa metodo main");
+    }
+    static void a(){
+        System.out.println("Entrou no metodo a");
+        b();
+        System.out.println("Saiu do metodo a");
+    }
+    static void b(){
+        System.out.println("Entrou no metodo b");
+        for(int i = 0; i <= 4; i++) System.out.print(i);
+        c();
+        System.out.println("Saiu do metodo b");
+    }
+    static void c(){
+        System.out.println("Entrou no metodo c");
+        System.out.println("Saiu do metodo c");
     }
 }
