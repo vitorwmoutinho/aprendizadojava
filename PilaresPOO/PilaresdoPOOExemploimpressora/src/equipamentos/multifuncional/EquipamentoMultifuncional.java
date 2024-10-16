c87df287bd3e858809ff904212acb211ef8cf74d
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-16 10:53:11 -0300

    Atualização e melhora do código

--- PilaresPOO/PilaresdoPOOExemploimpressora/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
+++ PilaresPOO/PilaresdoPOOExemploimpressora/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -1,8 +1,12 @@
 package equipamentos.multifuncional;
 
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
 public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
 
-	@Override
+	
 	public void Copiar() {
 		// TODO Auto-generated method stub
 		System.out.println("Copiando Multifuncional");
