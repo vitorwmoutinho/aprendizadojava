e09e7d1a041f7888afa1ad3163341cfa67e51243
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-17 10:57:56 -0300

    Definição da classe dispositivo para ser utilizada com a base para todos os dispositivos futuros.

--- projetoiphone/src/iphone/dispositivo/Dispositivo.java
+++ projetoiphone/src/iphone/dispositivo/Dispositivo.java
@@ -1,1 +1,20 @@
+package iphone.dispositivo;
 
+public abstract class Dispositivo {
+    protected String marca;
+    protected String modelo;
+
+    public Dispositivo(String marca, String modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+
+    public abstract void ligar();
+    public abstract void desligar();
+}
